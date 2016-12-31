d74cc2af1273b8cd840f0e4215920911aa92400f
Travis Giggy <travis@twosixcapital.com>
2016-12-30 19:24:50 -0800

    v1 for Add Place page

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, 
+                jsonify, request, redirect
 import requests
 import json
+import datetime
+import boto3
 import vars
+
 
 ##########
 # CONFIG #
@@ -23,6 +27,7 @@ class Place:
         self.ne_lng = ne_lng
         self.sw_lat = sw_lat
         self.sw_lng = sw_lng
+        self.insert_date = datetime.datetime.now()
 
     def img_url(self):
         img_bounds = "%s,%s|%s,%s|%s,%s|%s,%s|%s,%s" % (self.ne_lat, self.ne_lng, self.sw_lat, self.ne_lng, self.sw_lat, self.sw_lng, self.ne_lat, self.sw_lng, self.ne_lat, self.ne_lng)
@@ -47,9 +52,62 @@ def add_place():
 # API #
 #######
 
+# call google geocode service and get the name and coords for a location
 @app.route('/_get_place/<place>')
 def get_place(place):
+    try:
+        place = get_google_place(place)
+
+        # see if this place already exists in the db
+        table = vars.dynamodb.Table('Place')
+        response = table.get_item(
+                Key={
+                    'place_id': place.place_id
+                }
+            )
+        # if an Item was returned, this place is already in the db
+        try:
+            item = response['Item']
+            return jsonify("place already in database")
+
+        # if a KeyError is thrown, this place is not in the database
+        except KeyError:
+            return jsonify(place.img_url())
+
+    # google returned no results
+    except:
+        return jsonify("google returned no results")
+
+# insert a place into database
+@app.route('/_insert_place/', methods=['POST'])
+def insert_place():
+
+    str_place = request.form['hidden_place']
     
+    place = get_google_place(str_place)
+    table = vars.dynamodb.Table('Place')
+    table.put_item(
+            Item={
+                'place_id': place.place_id,
+                'name': place.name,
+                'lat': str(place.lat),
+                'lng': str(place.lng),
+                'ne_lat': str(place.ne_lat),
+                'ne_lng': str(place.ne_lng),
+                'sw_lat': str(place.sw_lat),
+                'sw_lng': str(place.sw_lng),
+                'insert_date': str(place.insert_date)
+            }
+        )
+
+    return redirect("/")
+    
+
+###########
+# HELPERS #
+###########
+
+def get_google_place(place):
     # call google api and bring it on back
     google_result = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' % (place, vars.google_api_key_geocode))
     """
@@ -59,17 +117,15 @@ def get_place(place):
     r.content                   json returned from api
     r.json()                    json returned from api
     """
-    r = {}
+
     r = google_result.json()['results'][0]
-    
+
     place = Place(r['place_id'], r['formatted_address'], 
                     r['geometry']['location']['lat'], r['geometry']['location']['lng'], 
                     r['geometry']['bounds']['northeast']['lat'], r['geometry']['bounds']['northeast']['lng'],
                     r['geometry']['bounds']['southwest']['lat'], r['geometry']['bounds']['southwest']['lng'])
     
-
-    return jsonify(place.img_url())
-
+    return place
 
 #######
 # RUN #
