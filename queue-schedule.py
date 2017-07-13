5d957c7695788f3245e707dc45e0f20da139cbcb
Travis Giggy <travis@twosixcapital.com>
2017-07-13 14:33:27 -0700

    don't be so wasteful

--- queue-schedule.py
+++ queue-schedule.py
@@ -20,13 +20,6 @@ def get_server():
     else:
         return 'http://redata.giggy.com'
 
-# def main():
-#     # if no messages exist in queue, see if there are any places that need to be queued
-#     queue_place()
-
-#     # process queue if there are sqs messages in it
-#     #process_queue()
-
 def queue_place():
     # call a page that will queue up a place if it needs to be queued
     server = get_server()
@@ -44,22 +37,24 @@ def process_queue():
 
     print 'Message: %s\nMessages in Queue: %s\n-----' % (data['message'], data['message_count'])
 
-    # if data['message_count'] != '0':
-    #     time.sleep(3)
-    #     get_queue()
+    if data['message_count'] != '0':
+        time.sleep(3)
+        process_queue()
 
 ###########################
 
 # clear existing schedules
 schedule.clear()
-# check if there are new messages in sqs queue every 6 hours
+# check if a new place needs to be queued every 6 hours
 schedule.every(6).hours.do(queue_place)
-schedule.every(3).seconds.do(process_queue)
+# kick off queue processing every 6 hours
+schedule.every(6).hours.do(process_queue)
 
 queue_place()
+process_queue()
 
 while True:
     #print '...tick...'
     schedule.run_pending()
-    time.sleep(3) # check every hour whether any jobs need to run
+    time.sleep(3600) # check every hour whether any jobs need to run
 
